efd72f22fd7c8f62c70c770823974497888e94ff
アルゴ・リズム <arugorizumu@ip-10-88-9-68.ap-northeast-1.compute.internal>
2026-05-25 10:50:42 +0900

    初回アップ

---
+++ AsagaoGameGUI.java
@@ -0,0 +1,348 @@
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.util.Random;
+
+public class AsagaoGameGUI extends JFrame {
+
+    CardLayout cardLayout = new CardLayout();
+    JPanel mainPanel = new JPanel(cardLayout);
+
+    double growth = 0;
+    int water = 50;
+    int day = 1;
+    int noWaterDays = 0;
+
+    int lastWater = 50;
+    double lastGrowthRate = 1.0;
+
+    boolean isDead = false;
+    boolean isBloomed = false;
+
+    LocalDate date = LocalDate.of(2026, 4, 1);
+    Random random = new Random();
+
+    String currentWeather = "晴れ";
+
+    JLabel imageLabel;
+    JLabel statusLabel;
+
+    JButton waterButton;
+    JButton skipButton;
+
+    public AsagaoGameGUI() {
+
+        setTitle("あさがお育成ゲーム");
+        setSize(900, 520);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createStartScreen();
+        createGameScreen();
+
+        add(mainPanel);
+
+        cardLayout.show(mainPanel, "START");
+
+        setVisible(true);
+    }
+
+    void createStartScreen() {
+
+        JPanel startPanel = new JPanel(new BorderLayout());
+
+        JLabel startImage = new JLabel(new ImageIcon("start.jpg"));
+        startImage.setHorizontalAlignment(JLabel.CENTER);
+
+        JButton startButton = new JButton("はじめる");
+        startButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+
+        startButton.addActionListener(e -> {
+            cardLayout.show(mainPanel, "GAME");
+        });
+
+        startPanel.add(startImage, BorderLayout.CENTER);
+        startPanel.add(startButton, BorderLayout.SOUTH);
+
+        mainPanel.add(startPanel, "START");
+    }
+
+    void createGameScreen() {
+
+        JPanel gamePanel = new JPanel(new BorderLayout());
+
+        statusLabel = new JLabel();
+        statusLabel.setFont(new Font("SansSerif", Font.BOLD, 15));
+        statusLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        imageLabel = new JLabel();
+        imageLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        waterButton = new JButton("水をあげる");
+        skipButton = new JButton("日付を進める");
+
+        waterButton.addActionListener(e -> {
+
+            water += 5;
+
+            if (water > 100) {
+                water = 100;
+            }
+
+            noWaterDays = 0;
+
+            updateScreen();
+        });
+
+        skipButton.addActionListener(e -> {
+
+           lastWater = water;
+           lastGrowthRate = getWaterGrowthRate(lastWater);
+
+           date = date.plusDays(1);
+           day++;
+
+           currentWeather = getWeather();
+
+           boolean showWaterMessage = false;
+
+           passOneDay();
+
+           updateScreen();
+        });
+
+        JPanel buttonPanel = new JPanel();
+
+        buttonPanel.add(waterButton);
+        buttonPanel.add(skipButton);
+
+        gamePanel.add(statusLabel, BorderLayout.NORTH);
+        gamePanel.add(imageLabel, BorderLayout.CENTER);
+        gamePanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        mainPanel.add(gamePanel, "GAME");
+
+        updateScreen();
+    }
+
+    void updateScreen() {
+
+        statusLabel.setText(
+                getSeason() + " " + getSeasonDay() + "日目"
+                        + "　天気：" + currentWeather
+                        + "　成長段階：" + getGrowthStage()
+                        + "　成長：" + String.format("%.1f", growth) + "%"
+                        + "　水分：" + water + "%"
+                        + "　判定水分：" + lastWater + "%"
+                        + "　成長率：" + String.format("%.1f", lastGrowthRate)
+        );
+
+        if (showWaterMessage) {
+
+            imageLabel.setIcon(new ImageIcon("watered.jpg"));
+
+        } else {
+
+            String imageName = getGrowthImage();
+
+            imageLabel.setIcon(new ImageIcon(imageName));
+        }
+
+        if (isBloomed) {
+
+            JOptionPane.showMessageDialog(this,
+                    "あさがおの花が咲きました！");
+
+            waterButton.setEnabled(false);
+            skipButton.setEnabled(false);
+        }
+
+        if (isDead) {
+
+            JOptionPane.showMessageDialog(this,
+                    "あさがおは枯れてしまいました……");
+
+            waterButton.setEnabled(false);
+            skipButton.setEnabled(false);
+        }
+    }
+
+    void passOneDay() {
+
+        if (currentWeather.equals("雨")) {
+            water += 25;
+        }
+
+        noWaterDays++;
+
+        if (noWaterDays == 1) {
+            water -= 5;
+        } else if (noWaterDays == 2) {
+            water -= 10;
+        } else {
+            water -= 20;
+        }
+
+        if (water > 100) {
+            water = 100;
+        }
+
+        if (water < 0) {
+            water = 0;
+        }
+
+        growth += lastGrowthRate;
+
+        if (growth < 0) {
+            growth = 0;
+        }
+
+        if (water <= 0) {
+            isDead = true;
+        }
+
+        if (growth >= 100) {
+            growth = 100;
+            isBloomed = true;
+        }
+    }
+
+    double getWaterGrowthRate(int targetWater) {
+
+        int difference = targetWater - 50;
+
+        if (difference == 0) {
+            return 1.0;
+        }
+
+        if (difference > 0) {
+            return 1.0 - (difference / 10) * 0.1;
+        } else {
+            return 1.0 - (Math.abs(difference) / 10) * 0.2;
+        }
+    }
+
+    String getWeather() {
+
+        int number = random.nextInt(100);
+
+        String season = getSeason();
+
+        if (season.equals("春")) {
+
+            if (number < 45) {
+                return "晴れ";
+            } else if (number < 75) {
+                return "曇り";
+            } else {
+                return "雨";
+            }
+
+        } else if (season.equals("夏")) {
+
+            if (number < 60) {
+                return "晴れ";
+            } else if (number < 80) {
+                return "曇り";
+            } else {
+                return "雨";
+            }
+
+        } else if (season.equals("秋")) {
+
+            if (number < 40) {
+                return "晴れ";
+            } else if (number < 75) {
+                return "曇り";
+            } else {
+                return "雨";
+            }
+
+        } else {
+
+            if (number < 30) {
+                return "晴れ";
+            } else if (number < 75) {
+                return "曇り";
+            } else {
+                return "雨";
+            }
+        }
+    }
+
+    String getSeason() {
+
+        int seasonCycle = ((day - 1) / 30) % 4;
+
+        if (seasonCycle == 0) {
+            return "春";
+        } else if (seasonCycle == 1) {
+            return "夏";
+        } else if (seasonCycle == 2) {
+            return "秋";
+        } else {
+            return "冬";
+        }
+    }
+
+    int getSeasonDay() {
+        return ((day - 1) % 30) + 1;
+    }
+
+    String getGrowthStage() {
+
+        if (growth < 10) {
+            return "種";
+        } else if (growth < 20) {
+            return "発芽";
+        } else if (growth < 30) {
+            return "双葉";
+        } else if (growth < 40) {
+            return "小さい芽";
+        } else if (growth < 50) {
+            return "成長中";
+        } else if (growth < 60) {
+            return "つる成長";
+        } else if (growth < 70) {
+            return "葉が増える";
+        } else if (growth < 80) {
+            return "つぼみ準備";
+        } else if (growth < 90) {
+            return "つぼみ";
+        } else if (growth < 100) {
+            return "開花";
+        } else {
+            return "満開";
+        }
+    }
+
+    String getGrowthImage() {
+
+        if (growth < 10) {
+            return "normal.jpg";
+        } else if (growth < 20) {
+            return "sprout.png";
+        } else if (growth < 30) {
+            return "futaba.png";
+        } else if (growth < 40) {
+            return "small.png";
+        } else if (growth < 50) {
+            return "grow.png";
+        } else if (growth < 60) {
+            return "vine.png";
+        } else if (growth < 70) {
+            return "leaf.png";
+        } else if (growth < 80) {
+            return "bud.png";
+        } else if (growth < 90) {
+            return "flower_bud.png";
+        } else if (growth < 100) {
+            return "flower.png";
+        } else {
+            return "fullflower.png";
+        }
+    }
+
+    public static void main(String[] args) {
+        new AsagaoGameGUI();
+    }
+}
